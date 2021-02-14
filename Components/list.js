4f085f07cc0c77eef373aca7e030d128266d4e9d
daya-0393 <61110559+daya-0393@users.noreply.github.com>
2021-02-13 20:02:37 -0600

    new changes

---
+++ Components/list.js
@@ -0,0 +1,30 @@
+import cn from 'classnames'
+import styles from '../styles/services.module.css'
+
+export default function List(props){
+
+    const htmlList = props.list.map((item, index)=>{
+        if(props.list != ""){
+            return(
+                <div className={cn("shadow", styles.item)} key={index}>
+                    <p>{item}</p>
+                    <div className={styles.btn} onClick={props.clicked}>
+                        <i className={cn("fas fa-angle-double-down", styles.fas)}></i>
+                    </div> 
+                    <div className={cn(styles.description, styles.hiden)}>
+                        <p>
+                            {props.itemDesc[index]}
+                        </p>
+                    </div> 
+                </div>
+        )}else{
+            return null
+        }
+    })
+
+    return(
+        <ul className={styles.list}>
+            {htmlList}
+        </ul>
+    )
+}
--- pages/servicio.js
+++ pages/servicio.js
@@ -3,60 +3,64 @@ import Header from '../Components/header'
 import Footer from '../Components/footer'
 import cn from 'classnames'
 import { useState, useEffect, useRef } from 'react'
-import {useRouter} from 'next/router'
-import {content} from '../lib/serviceContent'
+import { content } from '../lib/serviceContent'
+import List from '../Components/list'
 
 
-export default function Servicios(){
+export default function Servicios() {
     const [type, setType] = useState("");
-    const [htmlList, setHtmlList] = useState([]);
     const [heading, setHeading] = useState("");
     const [text, setText] = useState("");
+    const [list, setList] = useState([""]);
+    const [itemDesc, setItemDesc] = useState([]);
+    const [htmlList, setHtmlList] = useState([]);
     const listItem = useRef();
 
-    useEffect(()=>{
-        console.log(sessionStorage.getItem('type'))
+
+    useEffect(() => {
         setType(sessionStorage.getItem('type'));
-        setHeading(sessionStorage.getItem('heading'));
-        setText(sessionStorage.getItem('text'));
-    })
+        console.log(type);
+    }, [type])
 
-    const expandShadow= (e) =>{
-        console.log(e.target);
-    }
-
-    const hideShadow= (e) =>{
-        console.log(e.target.key);
-    }
-
-    useEffect(()=>{
-        if(sessionStorage.list != null || sessionStorage != ''){
-            setHtmlList(sessionStorage.list.split(',').map((item, index)=>{
-                return(
-                    <div className={cn("shadow", styles.item)} key={index} ref={listItem} onMouseEnter={expandShadow} onMouseLeave={hideShadow}>
-                        <p>{item}</p>
-                        <i className={cn("fas fa-angle-double-down", styles.fas)}></i>
-                    </div> 
-                ) 
-                
-            }));
+    useEffect(() => {
+        if (type.length != 0) {
+            console.log("entra")
+            setSSvalues();
+            getSSvalues();
         }
-    },[])
+    }, [type])
 
-    const setServiceContent = ()=>{
+    const setSSvalues = () => {
+        sessionStorage.setItem('list', content[type].list);
         sessionStorage.setItem('heading', content[type].heading);
         sessionStorage.setItem('text', content[type].text);
-        sessionStorage.setItem('list', content[type].list);
+        sessionStorage.setItem('itemDesc', content[type].itemDesc);
     }
-    
-    return(
-        <div onLoad={setServiceContent} className="wrapper">
+    const getSSvalues = () => {
+        if(sessionStorage.getItem('list') != ""){
+            setList((sessionStorage.getItem('list')).split(','));
+            setItemDesc((sessionStorage.getItem('itemDesc')).split(','))
+        }
+        setHeading(sessionStorage.getItem('heading'));
+        setText(sessionStorage.getItem('text'));
+    }
+
+    const handleItemClick = ()=>{
+
+    }
+
+    return (
+        <div className="wrapper">
             <Header></Header>
-            <main className={cn("container", styles.container)}>
-                <h1 className={cn("heading", styles.heading)}>{heading}</h1>
-                <p className={styles.text}>{text}</p>
-                <div className={styles.list}>
-                    {htmlList}
+            <main className={styles.container}>
+                <div className={cn(styles.col, styles.left)}>
+                    <h1 className={cn("heading", styles.heading)}>{heading}</h1>
+                    <p className={styles.text}>{text}</p>
+                </div>
+                <div className={cn(styles.col, styles.right)}>
+                    <div className={styles.list}>
+                        <List list={list} itemDesc={itemDesc}></List>
+                    </div>
                 </div>
             </main>
             <Footer></Footer>
